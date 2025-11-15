c57964ae6f7a555a10bad8361dc3882e2fa0d7f2
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-11-15 20:00:48 +0530

    Making cors origin universal

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 
 const users = [
